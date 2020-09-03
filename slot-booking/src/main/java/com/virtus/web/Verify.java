eaeec9cbcadce4c003741aab8bb9c5649b99e7b5
Vandana2220 <66233743+Vandana2220@users.noreply.github.com>
2020-09-03 10:12:41 +0530

    Final Changes

--- slot-booking/src/main/java/com/virtus/web/Verify.java
+++ slot-booking/src/main/java/com/virtus/web/Verify.java
@@ -17,25 +17,19 @@ public class Verify extends HttpServlet {
 		try {
 			String enteredOTP=req.getParameter("otp");
 			HttpSession session=req.getSession(false);  
-							
+			System.out.println("fasdaff");
+				
 	        String n=(String)session.getAttribute("OTP");
 			if(n.equals(enteredOTP)) {
-				/*Cookie ck=new Cookie("Successful","True");//creating cookie object
-                ck.setMaxAge(15000);
-                res.addCookie(ck); */
 				session.removeAttribute("OTP");
 				res.sendRedirect("/slot-booking/disp.jsp");
 			}
 			else {
 				req.setAttribute("successful","true");
-				RequestDispatcher rd=req.getRequestDispatcher("disp.jsp");
-				rd.forward(req,res);
 				res.sendRedirect("/slot-booking/otp.jsp");
 			}
 		}catch(Exception e) {
-			/*Cookie ck=new Cookie("Successful","False");//creating cookie object
-            ck.setMaxAge(15000);
-            res.addCookie(ck); */
+			
 			HttpSession session1=req.getSession(false);
 			session1.setAttribute("Message","Failed to send OTP");
 			res.sendRedirect("/slot-booking/details.jsp");
